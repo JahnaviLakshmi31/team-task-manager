1a1d3b1c153c1d81b0e0d9c20dfe866263da5eec
JahnaviLakshmi31 <jahnavilakshmi3103@gmail.com>
2026-05-16 10:55:18 +0530

    deployment ready

--- server/index.js
+++ server/index.js
@@ -36,5 +36,5 @@ app.get("/profile", authMiddleware, (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log("Server Started");
+  console.log("Server Started on ${PORT}");
 });
